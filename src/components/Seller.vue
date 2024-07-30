bc9cf8ffa359de836cfd7c61289ee2f9466723b8
白熊想吃鱼 <3024131925@qq.com>
2024-07-30 16:06:50 +0800

    feat: 定时刷新

--- src/components/Seller.vue
+++ src/components/Seller.vue
@@ -5,49 +5,65 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, reactive, computed, onBeforeUnmount } from 'vue'
 import { defineComponent } from 'vue'
 import instance from '@/utils/request'
 import * as echarts from 'echarts'
 import type { ECharts } from 'echarts'
 
 // 定义图表数据的类型
-
 interface SellerData {
   name: string
   value: number
 }
+
 defineComponent({
   name: 'SellerComponent'
 })
 
 // 引用 DOM 元素
 const chart = ref<HTMLElement | null>(null)
-
 // 引用 ECharts 实例
 const chartInstance = ref<ECharts | null>(null)
+const chartData = ref<SellerData[]>([])
+const state = reactive({ currentPage: 1, pageSize: 5 })
+
+const totalPage = computed(() =>
+  Math.ceil(chartData.value.length / state.pageSize)
+)
 
 const initChart = () => {
   chartInstance.value = echarts.init(chart.value)
+  //鼠标移动移除效果
+  chartInstance.value.on('mouseover', () => {
+    clearInterval(timer.value)
+  })
+  chartInstance.value.on('mouseout', () => {
+    handleInterval()
+  })
 }
 
 //获取数据
-const chartData = ref<SellerData[]>([])
+
 const getData = async () => {
   try {
     const res = await instance.get<SellerData[]>('/seller')
-    chartData.value = res.data // 提取数据
+    chartData.value = res.data.sort((a, b) => a.value - b.value)
   } catch (error) {
     console.error('获取数据时出错:', error)
   }
-
   updated()
+  handleInterval()
 }
 
 const updated = () => {
-  const sellers = chartData.value.map((item) => item.name)
+  const showDate = chartData.value.slice(
+    (state.currentPage - 1) * state.pageSize,
+    state.currentPage * state.pageSize
+  )
+  const sellers = showDate.map((item) => item.name)
 
-  const values = chartData.value.map((item) => item.value)
+  const values = showDate.map((item) => item.value)
 
   const option = {
     title: {
@@ -69,6 +85,26 @@ const updated = () => {
   }
   chartInstance.value?.setOption(option)
 }
+
+//定时切换
+const timer = ref<number | undefined>(undefined)
+
+const handleInterval = () => {
+  if (timer.value) {
+    clearInterval(timer.value)
+  }
+  timer.value = setInterval(() => {
+    state.currentPage++
+    if (state.currentPage > totalPage.value) state.currentPage = 1
+    updated()
+  }, 3000)
+}
+
+//清除定时器
+onBeforeUnmount(() => {
+  clearInterval(timer.value)
+})
+
 onMounted(() => {
   initChart()
   getData()
